2d1bad1b256fa6957737f1a8690b31a0bb90eae0
Ryanson <sidusrs@gmail.com>
2023-11-30 02:31:29 +0900

    11-30

--- Bridge/Br4/L_691.cpp
+++ Bridge/Br4/L_691.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
 #define ll long long
 
+ll gcd(ll a, ll b)
+{
+    if(a>b) swap(a, b);
+
+    while(a != 0)
+    {
+        b = b%a;
+        swap(a, b);
+    }
+
+    return b;
+}
+
 int main() {
-    ll n; cin >> n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+    int ans = 0;
+    //k*(i^2-j^2), 2*k*i*j, k*(i^2+j^2)
+    //i>j>0, k>0,
+
+    char cnt[n/2+1] = {0};
+    for(int i=2; i*i+i<=n; i++)
+    {
+        for(int j=i-1; j>0; j-=2)
+        {
+            if(gcd(i, j) != 1) continue;
+
+            for(int k=1; 2*k*i*(i+j) <= n; k++) {
+                int a = k*i*(i+j);
+                cnt[a]++;
+                if(cnt[a]==1) ans++;
+                else if(cnt[a]==2) ans--;
+            }
+        }
+    }
+
+    cout << ans;
 }
--- Bridge/Br4/M_692.cpp
+++ Bridge/Br4/M_692.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <cmath>
 using namespace std;
 #define ll long long
-// 한 변 길이 최대 7,500,000
+
+ll gcd(ll a, ll b)
+{
+    if(a>b) swap(a, b);
+
+    while(a != 0)
+    {
+        b = b%a;
+        swap(a, b);
+    }
+
+    return b;
+}
 
 int main() {
-    ll n; cin >> n;
-    vector<ll> square;
-    vector<ll> ans; //가능한 L
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+    int ans = 0;
+    //k*(i^2-j^2), 2*k*i*j, k*(i^2+j^2)
+    //i>j>0, k>0,
+
+    char cnt[n/2+1] = {0};
+    for(int i=2; i*i+i<=n; i++)
+    {
+        for(int j=i-1; j>0; j-=2)
+        {
+            if(gcd(i, j) != 1) continue;
+
+            for(int k=1; 2*k*i*(i+j) <= n; k++) {
+                int a = k*i*(i+j);
+                cnt[a]++;
+                if(cnt[a]==1) ans++;
+                else if(cnt[a]==2) ans--;
+            }
+        }
+    }
+
+    cout << ans;
 }
