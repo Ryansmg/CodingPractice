af945a07b8b19a25c6f19bc28b43801e320c97b3
Mingi <ryansmg6496@gmail.com>
2024-01-10 19:49:34 +0900

    01-10 (2)

---
+++ Bridge/Br5-1/p1724_hack.cpp
@@ -0,0 +1,102 @@
+#define v vector
+#define vpat <vector>
+#define deff define
+#define swsnm sync_with_stdio
+#define c ,
+#define iopt <iostream>
+#define dt .
+#define sc ;
+#define msgn -
+#define kiok1 cin
+#define nl "\n"
+#define sp " "
+#define kiok2 cout
+#define varn int
+#include iopt
+#define x <<
+#define p cout x
+#define x2 >>
+#define kiok3 tie
+#define getnm varn n c m; kiok1 x2 n x2 m sc
+#define himp :
+#define pc %
+#define imp ::
+#include vpat
+#define ustdwoc std
+#define ustd ustdwoc imp
+#define z 0
+#define pan0 a
+#define defi if
+#define scj sc j
+#define on 1
+#define mon -on
+#define tw 2
+#define dnxb p ""
+#define m1 --
+#define p1 ++
+#define ls <
+#define osb [
+#define csb ]
+#define obb (
+#define cbb )
+#define otbb obb obb obb
+#define ctbb cbb cbb cbb
+#define cosb csb osb
+#define mc m1 c
+#define czc obb z cbb
+#define danm3 m
+#define k3x kiok3 czc sc
+#define pan1 b
+#define fst varn j is
+#define f for
+#define gr >
+#define ob {
+#define eb }
+#define is =
+#define eq ==
+#define goreq >=
+#define maf varn main obb cbb
+#define danm2 t
+#define tkx dt k3x
+#define accp osb j cosb i csb
+#define danm arr
+#define dneb dn eb
+#define sgs scj goreq z scj
+#define slns scj ls n scj
+#define ef else f
+#define dn dnxb sc
+#define pafed p b x sp sc dn p nl sc dneb
+#define defarr2 obb danm3 cbb sc v ls v ls
+#define f1fsc varn i is m mon sc
+#define fio ios_base imp swsnm czc sc kiok1 tkx kiok2 tkx
+#define defarr3 varn x2 danm obb n c t cbb sc
+#define use ustd dn using namespace ustdwoc sc dn
+#define defarr1 v ls varn gr danm2
+#define pa ob f obb auto pan1 himp pan0 cbb pafed
+#define dnxb2c dnxb c dnxb
+#define parr f obb auto a himp arr cbb ob pa dneb
+#define f2fsc i goreq z sc
+#define defarr defarr1 defarr2 defarr3
+#define dn3 dn dn dn
+#define enf2 mc dnxb2c c endf
+#define dnxb3c dnxb c dnxb c dnxb2c
+#define defk varn k is on sc
+#define endf dnxb2c c dnxb3c c dnxb
+#define enf3 mc dnxb3c c endf
+#define mf f obb f1fsc f2fsc i enf2 cbb
+#define cond defi otbb m msgn i cbb pc tw cbb eq z cbb
+#define accarr danm accp
+#define f1 f obb fst n mon sgs enf3 cbb
+#define dn2 dn dn dn3
+#define f2 obb fst z slns p1 cbb
+#define ikp1 is k p1 sc dn
+#define def defarr defk
+#define aikp accarr ikp1 eb
+#define bcb ob cond ob
+#define solve2 dneb
+#define f12 f1 aikp ef f2 aikp
+#define strt maf ob use fio getnm def
+#define ed parr dn2 solve2
+#define solve strt mf bcb f12 ed
+
+solve
