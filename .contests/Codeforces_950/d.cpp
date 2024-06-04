c42d1c3e663f4392bb20a13125a3578f6f469957
Mingi <ryansmg6496@gmail.com>
2024-06-04 13:45:02 +0900

    06-04 Codeforces_950

---
+++ .contests/Codeforces_950/d.cpp
@@ -0,0 +1,141 @@
+#define USE_MACROS true
+#define USE_INT false
+#define USE_INT128 true
+#define USE_DOUBLE false
+#define GCC_OPTIMIZE_ENABLE false
+#pragma region macros
+#if USE_MACROS
+#include <bits/stdc++.h>
+#if GCC_OPTIMIZE_ENABLE
+// #pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+#if !USE_INT
+#define int long long // set USE_INT to true to disable
+#endif
+#if !USE_DOUBLE
+#define double long double // set USE_DOUBLE to true to disable
+#endif
+#define llmax 9223372036854775807 // 2^63-1
+#if USE_INT
+#define intmax 2147483647
+#else
+#define intmax llmax
+#endif
+#define INF 1000000000000000000 // INF * INF > 2^63
+#define inf 3000000000 // inf > 2^31, inf * inf < 2^63
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr) // enables fast input/output
+#define filein freopen("C:/Users/ryans/Desktop/Coding/Baekjoon/input.txt", "r", stdin) // stdin <- Coding/Baekjoon/input.txt
+#define fileout freopen("C:/Users/ryans/Desktop/Coding/Baekjoon/output.txt", "w", stdout) // stdout <- Coding/Baekjoon/output.txt
+#define all(vec) (vec).begin(), (vec).end()
+#define forn(name, val) for(int name = 0; name < val; name++)
+#define forf(name, start, end) for(int name = start; name <= end; name++)
+#define pass print("") // do nothing
+
+using namespace std;
+template <typename T> using v = vector<T>;
+template <typename T> using v2 = v<v<T>>;
+template <typename T> using pq = priority_queue<T>;
+using ii = array<int, 2>;
+using iii = array<int, 3>;
+
+#if USE_INT128
+#define lint __int128 // to disable lint, set USE_INT128 to false
+#define ll long long
+lint LINTMAX = ((lint(1)<<126)-1)*2+1;
+string lint2str(const lint &i) {string ret,bs;if(i==-LINTMAX-1)return lint2str(i/10)+"8";if(!i)return "0";if(i<0)return "-"+lint2str(-i);
+	lint t=1; forn(as, 18)t*=10;lint a=i/(t*t);if(a){ret += to_string((ll) a);bs = to_string((ll) (i / t % (t * 10) + t));
+		forn(j, 18) ret += bs[j + 1];bs = to_string((ll) ((i % t) + t));forn(j, 18) ret += bs[j + 1];
+	} else {lint b = i / t % (t * 10);if (b) {ret += to_string((ll) b);bs = to_string((ll) ((i % t) + t));
+			forn(j, 18) ret += bs[j + 1];} else { ret += to_string((ll) (i % t)); }}return ret;}
+istream &operator>>(istream &in, lint &l) {string s;in>>s;lint t=l=0,size=s.size(),k=1;if(s[0]=='-')t=1;
+	for(lint i=size-1;i>=t;i--){if(!t)l+=(s[i]-'0')*k;else l-=(s[i]-'0')*k;k*=10;}return in;}
+ostream &operator<<(ostream &out,const lint &i){ out << lint2str(i); return out; }
+#endif
+
+template <typename T> void compress(v<T> &v, const bool &autosort=true) { if(autosort) sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <typename T> T idx(const T &val, const v<T> &compressed) { return lower_bound(all(compressed), val) - compressed.begin(); }
+template <typename T> T input() {T t; cin >> t; return t;} // explicit type, input<TYPE>()
+int input() { int t; cin >> t; return t;} // default type : int
+template <typename T> void print(const T &i, const string& end="") { cout << i << end; }
+template <typename T> T pow_(T a, T b) { return pow_(a, b, intmax); }
+template <typename T> T pow_(T a, T b, T mod) { a%=mod;T ans=1;while(b){if(b&1)ans=ans*a%mod;b>>=1;a=a*a%mod;} return ans; }
+template <typename T> T gcd_(T a, T b) { if(a<b) swap(a, b); while(b) { T r = a % b; a = b; b = r; } return a; }
+#endif
+#pragma endregion
+
+// d
+
+signed main() {
+    fastio;
+    int tc = input();
+    while(tc--) {
+        int n = input();
+        v<int> arr;
+        forn(i, n) arr.push_back(input());
+        v<int> gcdr;
+        forf(i, 1, n-1) gcdr.push_back(gcd_(arr[i-1], arr[i]));
+        int prev = -INF, ndec = 0, idx = -1, fidx, fused;
+        bool used = false, answered = false;
+        for(int i : gcdr) {
+            idx++;
+            if(prev <= i) {
+                if(ndec!=0 && used) {
+                    cout << "NO\n";
+                    answered = true;
+                    break;
+                }
+                if(ndec!=0) {
+                    fidx = idx-1;
+                    fused = ndec;
+                    used = true;
+                }
+                ndec = 0;
+            } else {
+                ndec++;
+                if(ndec==3) {
+                    cout << "NO\n";
+                    answered = true;
+                    break;
+                }
+            }
+            prev = i;
+        }
+        bool edge = false;
+        if(ndec != 0 && used) {
+            cout << "NO\n";
+            continue;
+        } else if(ndec!=0) {
+            fidx = idx;
+            fused = ndec;
+            used = true;
+            edge = true;
+        }
+        if(!answered) {
+            if(fused == 2) {
+                int g = gcd_(arr[fidx-1], arr[fidx+1]);
+                if(!(gcdr[fidx-2] <= g && (edge || g <= gcdr[fidx+1]))) {
+                    cout << "NO\n"; continue;
+                }
+            }
+            if(fused == 1) {
+                bool succeed = false;
+                int g;
+                if(!edge) {
+                    g = gcd_(arr[fidx], arr[fidx + 2]);
+                    if ((gcdr[fidx - 1] <= g && g <= gcdr[fidx + 2])) succeed = true;
+                }
+                g = gcd_(arr[fidx-1], arr[fidx+1]);
+                if(gcdr[fidx-2] <= g && (edge || g <= gcdr[fidx+1])) succeed = true;
+                if(!succeed) {
+                    cout << "NO\n"; continue;
+                }
+            }
+            cout << "YES\n";
+        }
+    }
+}
+
+// 1 1 1 1 1 1 1 1 1
+//  1 1 1 1 1 1 1 0
---
+++ .contests/Codeforces_950/e.cpp
@@ -0,0 +1,115 @@
+#define USE_MACROS true
+#define USE_INT false
+#define USE_INT128 true
+#define USE_DOUBLE false
+#define GCC_OPTIMIZE_ENABLE true
+#pragma region macros
+#if USE_MACROS
+#include <bits/stdc++.h>
+#if GCC_OPTIMIZE_ENABLE
+// #pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#endif
+#if !USE_INT
+#define int long long // set USE_INT to true to disable
+#endif
+#if !USE_DOUBLE
+#define double long double // set USE_DOUBLE to true to disable
+#endif
+#define llmax 9223372036854775807 // 2^63-1
+#if USE_INT
+#define intmax 2147483647
+#else
+#define intmax llmax
+#endif
+#define INF 1000000000000000000 // INF * INF > 2^63
+#define inf 3000000000 // inf > 2^31, inf * inf < 2^63
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr) // enables fast input/output
+#define filein freopen("C:/Users/ryans/Desktop/Coding/Baekjoon/input.txt", "r", stdin) // stdin <- Coding/Baekjoon/input.txt
+#define fileout freopen("C:/Users/ryans/Desktop/Coding/Baekjoon/output.txt", "w", stdout) // stdout <- Coding/Baekjoon/output.txt
+#define all(vec) (vec).begin(), (vec).end()
+#define forn(name, val) for(int name = 0; name < val; name++)
+#define forf(name, start, end) for(int name = start; name <= end; name++)
+#define pass print("") // do nothing
+
+using namespace std;
+template <typename T> using v = vector<T>;
+template <typename T> using v2 = v<v<T>>;
+template <typename T> using pq = priority_queue<T>;
+using ii = array<int, 2>;
+using iii = array<int, 3>;
+
+#if USE_INT128
+#define lint __int128 // to disable lint, set USE_INT128 to false
+#define ll long long
+lint LINTMAX = ((lint(1)<<126)-1)*2+1;
+string lint2str(const lint &i) {string ret,bs;if(i==-LINTMAX-1)return lint2str(i/10)+"8";if(!i)return "0";if(i<0)return "-"+lint2str(-i);
+	lint t=1; forn(as, 18)t*=10;lint a=i/(t*t);if(a){ret += to_string((ll) a);bs = to_string((ll) (i / t % (t * 10) + t));
+		forn(j, 18) ret += bs[j + 1];bs = to_string((ll) ((i % t) + t));forn(j, 18) ret += bs[j + 1];
+	} else {lint b = i / t % (t * 10);if (b) {ret += to_string((ll) b);bs = to_string((ll) ((i % t) + t));
+			forn(j, 18) ret += bs[j + 1];} else { ret += to_string((ll) (i % t)); }}return ret;}
+istream &operator>>(istream &in, lint &l) {string s;in>>s;lint t=l=0,size=s.size(),k=1;if(s[0]=='-')t=1;
+	for(lint i=size-1;i>=t;i--){if(!t)l+=(s[i]-'0')*k;else l-=(s[i]-'0')*k;k*=10;}return in;}
+ostream &operator<<(ostream &out,const lint &i){ out << lint2str(i); return out; }
+#endif
+
+template <typename T> void compress(v<T> &v, const bool &autosort=true) { if(autosort) sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <typename T> T idx(const T &val, const v<T> &compressed) { return lower_bound(all(compressed), val) - compressed.begin(); }
+template <typename T> T input() {T t; cin >> t; return t;} // explicit type, input<TYPE>()
+int input() { int t; cin >> t; return t;} // default type : int
+template <typename T> void print(const T &i, const string& end="") { cout << i << end; }
+template <typename T> T pow_(T a, T b) { return pow_(a, b, intmax); }
+template <typename T> T pow_(T a, T b, T mod) { a%=mod;T ans=1;while(b){if(b&1)ans=ans*a%mod;b>>=1;a=a*a%mod;} return ans; }
+template <typename T> T gcd_(T a, T b) { if(a<b) swap(a, b); while(b) { T r = a % b; a = b; b = r; } return a; }
+#endif
+#pragma endregion
+
+// e
+
+// 1 3 4 2
+// 2 4 1 3
+// 3 1 2 4
+// 3 2 3 1
+
+signed main() {
+    fastio;
+    int tc = input();
+    while(tc--) {
+        int n, m; cin >> n >> m;
+        v2<int> arr(n, v<int>()), arr2 = arr;
+        v<int> row, column;
+        row = column = v<int>(n*m+1);
+        v<set<int>> rows(n, set<int>()), columns(m, set<int>());
+        forn(i, n) {
+            forn(j, m) {
+                int t = input();
+                arr[i].push_back(t);
+                row[t] = i;
+                column[t] = j;
+                rows[i].insert(t);
+                columns[j].insert(t);
+            }
+        }
+        int available = true;
+        forn(i, n) {
+            int t = input();
+            int r = row[t]; arr2[i].push_back(t);
+            forn(j, m-1) {
+                int t2 = input(); arr2[i].push_back(t2);
+                if(!available) continue;
+                available = (rows[r].find(t2)!=rows[r].end());
+            }
+        }
+        forn(j, m) {
+            if(!available) break;
+            int t = arr2[0][j], c = column[t];
+            forn(i, n-1) {
+                if(!available) break;
+                int t2 = arr2[i+1][j];
+                available = columns[c].find(t2)!=columns[c].end();
+            }
+        }
+        cout << (available ? "YES" : "NO") << '\n';
+    }
+}
